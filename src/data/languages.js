3cfe472c669969c65f0edffa800564b04d2e6878
RebelDi <napalm30@icloud.com>
2021-02-11 21:02:07 -0600

    Updates

--- src/data/languages.js
+++
@@ -1,7 +0,0 @@
-
-const languages = [
-    { value: "English", label: "en_US" },
-    { value: "Turkish", label: "tr" },
-  ];
-  
-  export default languages;
--- src/data/poSpeech.js
+++
@@ -1,8 +0,0 @@
-
-const poSpeech = [
-    { value: "Noun", label: "noun" },
-    { value: "Verb", label: "verb" },
-    { value: "Adjective", label: "adjective" },
-  ];
-  
-  export default poSpeech;
