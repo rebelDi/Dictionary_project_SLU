30bf4019143292d64ca51bd15c57c6dcb2448331
rebelDi <napalm30@icloud.com>
2021-02-12 11:23:44 -0600

    Added Files

---
+++ src/data/poSpeech.js
@@ -0,0 +1,8 @@
+
+const poSpeech = [
+    { value: "Noun", label: "noun" },
+    { value: "Verb", label: "verb" },
+    { value: "Adjective", label: "adjective" },
+  ];
+  
+  export default poSpeech;
