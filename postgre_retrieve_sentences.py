f7685415839c62789b9b7d6fa475b0c3d6f0fcf8
psriraj17 <78946867+psriraj17@users.noreply.github.com>
2021-03-18 23:53:22 -0400

    Creating Database, tables and function for sentence retrieval

---
+++ postgre_retrieve_sentences.py
@@ -0,0 +1,20 @@
+
+#retrieve sentences based on the input word.
+import psycopg2
+
+
+
+def main():
+    #connection to database
+    conn_string = "host='localhost' dbname='postgres' user='postgres' password='Post$123'"
+    #print(conn_string)
+
+    con = psycopg2.connect(conn_string)
+    curs = con.cursor()
+    a = 'crane'
+    #executing the query for retrieval
+    curs.execute(f"select sentence from english where word='{a}'")
+    x= curs.fetchall()
+    print(x)
+
+main() # function call
