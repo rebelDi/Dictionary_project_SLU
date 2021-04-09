12b4257323ca2d074ac67d1d422617b9719b9681
psriraj17 <78946867+psriraj17@users.noreply.github.com>
2021-04-08 22:35:50 -0400

    Update postgre_retrieve_sentences.py

--- postgre_retrieve_sentences.py
+++ postgre_retrieve_sentences.py
@@ -4,19 +4,19 @@ import psycopg2
 
 
 
-def main():
+def main(word):
     #connection to database
     conn_string = "host='localhost' dbname='postgres' user='postgres' password='Post$123'"
     #print(conn_string)
 
     con = psycopg2.connect(conn_string)
     curs = con.cursor()
-    a = 'recommend' #input the required word
+    
     #executing the query for retrieval
-    curs.execute(f"select sentence from english where word='{a}'")
+    curs.execute(f"select sentence from english where word='{word}'")
     x= curs.fetchall()
     print(x)
 
 
 
-main() # function call
+#main() # function call
