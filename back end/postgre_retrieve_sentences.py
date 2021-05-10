9c9f0c3cdaa34be3bd32e028a2e96bf15736602c
RebelDi <31730559+rebelDi@users.noreply.github.com>
2021-05-10 11:25:00 -0500

    Raj update retrieve sentence

--- back end/postgre_retrieve_sentences.py
+++ back end/postgre_retrieve_sentences.py
@@ -3,22 +3,27 @@
 import psycopg2
 import nltk.stem as stem
 
-
-
-def postgre_retrieve_sentences():
-    #connection to database
-    #conn_string = "host='localhost' dbname='postgres' user='postgres' password='Post$123'"
+def postgre_retrieve_sentences(word,language):
     conn_string = "dbname= 'postgres' user='sdadmin@postgre-psd' host='postgre-psd.postgres.database.azure.com' password='Post$123' port='5432' "
-    print(conn_string)
-
+ 
     con = psycopg2.connect(conn_string)
     curs = con.cursor()
-    ls = stem.LancasterStemmer()
-    word = ls.stem("eyes")
-    #executing the query for retrieval
-    curs.execute(f"select sentence from english where word='{word}'")
-    x= curs.fetchall()
-    print(x)
-    return x
 
-# main() # function call
+    #word = ls.stem("eyes")
+    #word = "банковский"
+    #language = "russian"
+    #executing the query for retrieval
+    if language == "english":
+        ls = stem.LancasterStemmer()
+        word = ls.stem(word)
+        curs.execute(f"select sentence from {language} where word='{word}'")
+        x= curs.fetchall()
+        return x
+    if language == "russian":
+        curs.execute(f"select sentence from {language} where word='{word}'")
+        x = curs.fetchall()
+        return x
+    if language== "turkish":
+        curs.execute(f"select sentence from {language} where word='{word}'")
+        x = curs.fetchall()
+        return x
