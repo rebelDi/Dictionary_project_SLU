a801ca638cac4e423154cc2b39d9910d3c323e82
RebelDi <31730559+rebelDi@users.noreply.github.com>
2021-05-19 14:27:53 -0500

    Add files via upload

--- back end/File_Manager.py
+++ back end/File_Manager.py
@@ -8,16 +8,18 @@ class File_Manager:
     def __init__(self, language):
         self.language = language
 
-    # def save_element_to_file(self, element, filename):
-    #     pickle.dump(element, open(absolute_path + 'trained/' + self.language + '/' + filename, 'wb'))
-
-    # def load_element_from_file(self, filename):
-    #     return pickle.load(open(absolute_path + 'trained/' + self.language + '/' + filename, 'rb'))
     def save_element_to_file(self, element, filename):
-        with bz2.BZ2File(absolute_path + 'trained/' + self.language + '/' + filename + '.pbz2', 'wb') as f:
-            cPickle.dump(element, f)
-        # pickle.dump(element, open(, 'wb'))
+        pickle.dump(element, open(absolute_path + 'trained/' + self.language + '/' + filename, 'wb'))
 
     def load_element_from_file(self, filename):
-        data = bz2.BZ2File(absolute_path + 'trained/' + self.language + '/' + filename + '.pbz2', 'rb')
-        return cPickle.load(data)
+        return pickle.load(open(absolute_path + 'trained/' + self.language + '/' + filename, 'rb'))
+    # def save_element_to_file(self, element, filename):
+    #     with bz2.BZ2File(absolute_path + 'trained/' + self.language + '/' + filename + '.pbz2', 'wb') as f:
+    #         cPickle.dump(element, f)
+    #     # pickle.dump(element, open(, 'wb'))
+
+    # def load_element_from_file(self, filename):
+    #     print(absolute_path + 'trained/' + self.language + '/' + filename + '.pbz2')
+    #     data = bz2.BZ2File(absolute_path + 'trained/' + self.language + '/' + filename + '.pbz2', 'rb')
+    #     print(cPickle.load(data))
+    #     return cPickle.load(data)
---
+++ back end/nltk_settings.py
@@ -0,0 +1,5 @@
+import nltk
+
+nltk.download('punkt') # pretrained tokenizer
+nltk.download("stopwords") # remove words like and, the, an, a, 
+nltk.download("stopwords") 
