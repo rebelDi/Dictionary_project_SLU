cf55dbf46d05b45d0b6b9754d76e97c20500fd1b
RebelDi <31730559+rebelDi@users.noreply.github.com>
2021-05-19 16:54:53 -0500

    Update nltk_settings.py

--- back end/nltk_settings.py
+++ back end/nltk_settings.py
@@ -2,4 +2,4 @@ import nltk
 
 nltk.download('punkt') # pretrained tokenizer
 nltk.download("stopwords") # remove words like and, the, an, a, 
-nltk.download("stopwords") 
+nltk.download("averaged_perceptron_tagger") 
